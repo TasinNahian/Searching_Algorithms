149d472741a82caea792b45ebdb9f6097b51220a
tasinnahian <tasin.nahian@gmail.com>
2022-07-25 03:25:59 +0600

    recursive algo for linear search

---
+++ Linear_Search/Linear_Search_Recursive_Approach.java
@@ -0,0 +1,32 @@
+package Searching_Algorithms.Linear_Search;
+import java.io.*;
+
+public class Linear_Search_Recursive_Approach 
+{
+    public static int linearSearch(int arr[], int size, int key)
+    {
+        if(size == 0){
+            return -1;
+        }
+        if(arr[size-1] == key)
+        {
+            return size-1;
+        }
+        else
+        {
+            int ans = linearSearch(arr, size-1, key);
+            return ans;
+        }
+    }
+    public static void main(String args[])
+    {
+        int arr [] = {1,2,3,4,5,6,7,8,9,10};
+        int ans = linearSearch(arr, arr.length, 7);
+        if(ans == -1)
+        {
+            System.out.println("The number is not in the array or array size is 0");
+        }
+        
+        System.out.println("The number is at position :" + ans);
+    }
+}
